cd0ffb54e1e9bb99accc3f3d3ac91c3c749ff5c6
Jacques Douglas <jacques.wsa@gmail.com>
2024-11-26 00:05:59 -0300

    Primary commit

---
+++ ship_scene/star_ship_image.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = (Input.get_axis("ui_up","ui_down")*8)
+	print(direction)
+	position.y += direction
